a9e52c5e9ee5612f2fac7975a5abf737b74ea1e7
Diogo Santana <diogosantanafreitasuna@gmail.com>
2023-09-25 15:56:36 -0300

    feat: utilizando provedores dos contextos

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import { RouterProvider } from "react-router-dom"
 import { router } from "./routes"
+import './App.css'
+import { ChampionshipProvider } from "./contexts/championshipContext"
+import { PlayerProvider } from "./contexts/playersContext"
+import { TeamProvider } from "./contexts/teamsContext"
 
-export function App () {
-  <>
-    <RouterProvider router={router}/>
-  </>
+export default function App () {
+  return (
+    <div className="main">
+      <ChampionshipProvider>
+        <TeamProvider>
+          <PlayerProvider>
+            <RouterProvider router={router}/>
+          </PlayerProvider>
+        </TeamProvider>
+      </ChampionshipProvider>,
+    </div>
+  )
 }
