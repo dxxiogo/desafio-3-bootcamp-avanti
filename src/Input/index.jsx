5323770760a6809490463dc82a45ebffcd19979a
Diogo Santana <diogosantanafreitasuna@gmail.com>
2023-09-25 19:56:35 -0300

    fix: remoção de arquivos desnecessários

--- src/Input/index.jsx
+++
@@ -1,8 +0,0 @@
-export function Input ({id, label, type, value, handleChange}) {
-    return (
-        <div>
-            <label htmlFor={id}>{label}</label>
-            <input  id={id} type={type} onChange={handleChange} value={value}/>
-        </div>
-    )
-}
