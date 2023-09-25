b602281e0660ac32567cbe8f5e457d701fa9ea9a
Diogo Santana <diogosantanafreitasuna@gmail.com>
2023-09-25 15:54:51 -0300

    feat: adicionando requisições

---
+++ src/api/api.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+export async function createRequest(data, path) {
+    try {
+      const response = await axios({
+        url: `http://localhost:3333/${path}`,
+        method:'POST',
+        data: data
+      });
+      console.log(response.data)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+export async function deleteRequest(path) {
+    try {
+      const response = await axios.delete(`http://localhost:3333/${path}`);
+      console.log(response.data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  
+
+
+export async function fetchRequest (path) {
+    const response = await axios.get(`http://localhost:3333/${path}`);
+    return response.data;
+}
