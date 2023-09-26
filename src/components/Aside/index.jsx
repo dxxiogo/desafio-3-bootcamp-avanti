70f580a58f04555fd83ffc886c5888c284211424
Diogo Santana <diogosantanafreitasuna@gmail.com>
2023-09-26 20:46:32 -0300

    style: destacando seção atual selecionada

--- src/components/Aside/index.jsx
+++ src/components/Aside/index.jsx
@@ -1,17 +1,44 @@
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 
 export function Aside () {
+
+    const {pathname} = useLocation();
+
     return (
         <aside>
             <nav>
                 <ul style={{display: 'flex', flexDirection: 'column', gap: '1rem'}}>
-                    <Link to={'/'} className="hover:bg-gray-100 p-1">Campeonatos</Link>
-                    <Link to={'players'} className="hover:bg-gray-100 p-1" >Jogadores</Link>
-                    <Link to={'teams'} className="hover:bg-gray-100 p-1">Times</Link>
+                    <Link 
+                        to={'/'} 
+                        className={`${pathname === "/" ? "bg-blue-500 text-white" : "hover:bg-gray-100"} rounded px-1 py-2`}
+                        >Campeonatos
+                    </Link>
+                    <Link 
+                        to={'players'} 
+                        className={`${pathname === "/players" ? "bg-blue-500 text-white" : "hover:bg-gray-100"} rounded px-1 py-2`} 
+                        >Jogadores
+                    </Link>
+                    <Link 
+                        to={'teams'} 
+                        className={`${pathname === "/teams" ? "bg-blue-500 text-white" : "hover:bg-gray-100"} rounded px-1 py-2`}
+                        >Times
+                    </Link>
                     <hr />
-                    <Link to={'createChampionship'} className="hover:bg-gray-100 p-1">Adicionar Campeonato</Link>
-                    <Link to={'createTeam'} className="hover:bg-gray-100 p-1">Adicionar Time</Link>
-                    <Link to={'createPlayer'} className="hover:bg-gray-100 p-1">Adicionar Jogador</Link>     
+                    <Link 
+                        to={'createChampionship'} 
+                        className={`${pathname === "/createChampionship" ? "bg-blue-500 text-white" : "hover:bg-gray-100"} rounded px-1 py-2`}
+                        >Adicionar Campeonato
+                    </Link>
+                    <Link 
+                        to={'createTeam'} 
+                        className={`${pathname === "/createTeam" ? "bg-blue-500 text-white" : "hover:bg-gray-100"} rounded px-1 py-2`}
+                        >Adicionar Time
+                    </Link>
+                    <Link 
+                        to={'createPlayer'} 
+                        className={`${pathname === "/createPlayer" ? "bg-blue-500 text-white" : "hover:bg-gray-100"} rounded px-1 py-2`}
+                        >Adicionar Jogador
+                    </Link>     
                 </ul>
             </nav>
         </aside>
