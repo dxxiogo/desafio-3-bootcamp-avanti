69e1e00e9c3200a4b8ac188c6d2a566b86b66ea8
Diogo Santana <diogosantanafreitasuna@gmail.com>
2023-09-26 10:36:46 -0300

    style: estilizando componentes de botão

--- src/components/DeleteButton/index.jsx
+++ src/components/DeleteButton/index.jsx
@@ -1,6 +1,6 @@
 export function DeleteButton ({handleClick}) {
     return (
-        <button class="p-2 bg-red-500 text-white rounded-md hover:bg-red-600" onClick={handleClick}>
+        <button className="p-2 bg-red-500 text-white rounded-md hover:bg-red-600" onClick={handleClick}>
         Excluir
       </button>
     )
--- src/components/ViewButton/index.jsx
+++ src/components/ViewButton/index.jsx
@@ -1,6 +1,6 @@
 export function ViewButton ({content, handleClick}) {
     return (
-        <button onClick={handleClick} class="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600">
+        <button onClick={handleClick} className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600">
             {content}
           </button>
     )
