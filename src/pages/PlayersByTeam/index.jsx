7d1ef106629a01673ef60261ba59368ad8475128
Diogo Santana <diogosantanafreitasuna@gmail.com>
2023-09-27 20:04:34 -0300

    feat: obtendo nome do time

--- src/pages/PlayersByTeam/index.jsx
+++ src/pages/PlayersByTeam/index.jsx
@@ -2,17 +2,24 @@ import { Outlet, useParams } from "react-router-dom"
 import { usePlayer } from "../../contexts/playersContext";
 import { useEffect } from "react";
 import PlayerInfo from "../../components/PlayerInfo";
+import { useTeam } from "../../contexts/teamsContext";
 
 export function PlayersByTeam () {
 
     const {players, fetchPlayers} = usePlayer();
+    const {teams} = useTeam();
     const {teamId} = useParams();
 
+    
     useEffect(() => {
-        fetchPlayers();     
+        fetchPlayers(); 
     }, [])
     
- 
+    
+    function getTeamName(teamId) {
+        const team = teams.find(({id}) => id === teamId);
+        return team.nome
+    }
     
     const playersFound = players.filter(({timeId}) => teamId === timeId);
 
@@ -22,7 +29,7 @@ export function PlayersByTeam () {
     return (
         <div>
             <div>
-                <h1 className="">Jogadores</h1>
+                <h1 className="text-2xl font-bold" >Jogadores do {getTeamName(teamId)}</h1>
                 <div className="max-h-72 overflow-y-auto mt-8">
                     {playersFound.map(({id, nome, idade, position}) => (
                         <PlayerInfo key={id} name={nome} age={idade} position={position} id={id}/>
