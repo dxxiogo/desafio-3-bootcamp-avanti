5c47e150a9d8abb9e4b1f0c36fbeb4e6d5e536c2
Diogo Santana <diogosantanafreitasuna@gmail.com>
2023-09-27 11:06:47 -0300

    feat: adicionando campo de busca para jogador

--- src/pages/Players/index.jsx
+++ src/pages/Players/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import PlayerInfo from "../../components/PlayerInfo"
 import { usePlayer } from "../../contexts/playersContext"
 
@@ -18,7 +18,13 @@ export function Players () {
 
     return (
         <div>
-            <h1 className="">Jogadores</h1>
+            <div className="flex justify-between">
+                <h1 className="text-2xl font-bold">Jogadores</h1>
+                <input 
+                    type="text" 
+                    placeholder="Digite um jogador"
+                    className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"/>
+            </div>
             <div className="max-h-72 overflow-y-auto mt-8">
                 {players.map(({id, nome, idade, position}) => (
                     <PlayerInfo key={id} name={nome} age={idade} position={position} id={id}/>
