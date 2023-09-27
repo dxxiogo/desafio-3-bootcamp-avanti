b0ded68c70c0e46dd879a1b0ff050de4fbff243a
Diogo Santana <diogosantanafreitasuna@gmail.com>
2023-09-27 20:01:17 -0300

    refactor: adicionando label para o Select

--- src/components/Select/index.jsx
+++ src/components/Select/index.jsx
@@ -1,10 +1,10 @@
-export function Select({ options, id, value, handleChange, ...rest}) {
+export function Select({ options, label, id, value, handleChange, ...rest}) {
     
     if(options)
         return (
         <div className="mb-4">
             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor={id}>
-            Times:
+                {label}
             </label>
             <select
             className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${rest.className}`}
