1de8d58608ae0c2e545208428520c81954cbb553
Diogo Santana <diogosantanafreitasuna@gmail.com>
2023-09-27 19:59:42 -0300

    feat: adicionando filtragem de jogadores por nome

--- src/pages/Players/index.jsx
+++ src/pages/Players/index.jsx
@@ -1,35 +1,42 @@
-import { useEffect, useState } from "react";
-import PlayerInfo from "../../components/PlayerInfo"
-import { usePlayer } from "../../contexts/playersContext"
+import React, { useEffect, useState } from "react";
+import PlayerInfo from "../../components/PlayerInfo";
+import { usePlayer } from "../../contexts/playersContext";
 
+export function Players() {
+  const { players, fetchPlayers } = usePlayer();
+  const [searchPlayer, setSearchPlayer] = useState("");
+  const [filteredPlayers, setFilteredPlayers] = useState(players);
 
-export function Players () {
+  useEffect(() => {
+    fetchPlayers();
+  }, []);
 
-    const {players, fetchPlayers} = usePlayer();
-    const [searchTerm, setSearchTerm] = useState("");
+  function filterPlayers(value) {
+    setSearchPlayer(value); 
+    setFilteredPlayers((state) => {
+      return players.filter(({ nome }) =>
+        nome.toUpperCase().includes(value.toUpperCase())
+      );
+    });
+  }
 
-   useEffect(() => {
-        fetchPlayers();
-   }, []);
-
-   const filteredPlayers = players.filter(({ nome }) =>
-    nome.toLowerCase().includes(searchTerm.toLowerCase())
+  return (
+    <div>
+      <div className="flex justify-between">
+        <h1 className="text-2xl font-bold">Jogadores</h1>
+        <input
+          value={searchPlayer}
+          onChange={(ev) => filterPlayers(ev.target.value)}
+          type="text"
+          placeholder="Digite um jogador"
+          className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        />
+      </div>
+      <div className="max-h-72 overflow-y-auto mt-8">
+        { filteredPlayers.map(({ id, nome, idade, position }) => (
+          <PlayerInfo key={id} name={nome} age={idade} position={position} id={id} />
+        ))}
+      </div>
+    </div>
   );
-
-    return (
-        <div>
-            <div className="flex justify-between">
-                <h1 className="text-2xl font-bold">Jogadores</h1>
-                <input 
-                    type="text" 
-                    placeholder="Digite um jogador"
-                    className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"/>
-            </div>
-            <div className="max-h-72 overflow-y-auto mt-8">
-                {players.map(({id, nome, idade, position}) => (
-                    <PlayerInfo key={id} name={nome} age={idade} position={position} id={id}/>
-                ))}
-            </div>
-        </div>
-    )
 }
